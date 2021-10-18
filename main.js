e4d6382e9099458845ddc4bd88ba81e3c7c2e4ef
marijanagardovic <marijana.gardovic99@gmail.com>
2021-10-18 15:57:28 +0200

    chnaged style for bookmark button

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 const hamburger = document.querySelector('#hamburger');
 const navUl = document.querySelector('#menu-ul');
+const bookmark = document.querySelector('#bookmark');
+const book =document.querySelector('.book');
+
+// hamburger
 
 hamburger.addEventListener('click', () => {
     navUl.classList.toggle('show');
-    
-})
+});
+
+//bookmark button
+
+bookmark.addEventListener("click", () => {
+    bookmark.classList.toggle("active");
+    if (bookmark.classList.contains("active")) {
+        book.innerHTML = "Bookmarked";
+    } else {
+        book.innerHTML = "Bookmark";
+    };
+});
